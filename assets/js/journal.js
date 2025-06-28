ccef443c7d10b9e5022a6f8cfb7baa7286779394
Avishkar <avishkarambade9@gmail.com>
2025-06-28 19:51:53 +0530

    add journal page

---
+++ assets/js/journal.js
@@ -0,0 +1,31 @@
+fetch("../data/journal.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const container = document.querySelector(".card-section");
+
+    data.map((post) => {
+      const newPost = document.createElement("div");
+      newPost.classList.add("card");
+    //   newPost.classList.add(`${post.categories[1]}`);
+      const imgSrc = `${post.image}`;
+      console.log(post.image);
+      newPost.innerHTML = `
+        
+        <div class="img-section">
+          <img
+            src="${post.image}"
+            alt="somthig went wrong"
+          />
+        </div>
+        <div class="card-contant">
+          <p class="card-heading">${post.username}</p>
+          <p class="card-text">
+            ${post.review}
+          </p>
+        </div>
+    
+      `;
+
+      container.appendChild(newPost);
+    });
+  });
