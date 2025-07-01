a2e2467da6687aff743424dee194377705d0500e
Avishkar <avishkarambade9@gmail.com>
2025-07-01 12:15:34 +0530

    optimaise login js page logic

--- Login page/login.js
+++ Login page/login.js
@@ -1,19 +1,14 @@
-console.log("login page");
-let eyeC = document.querySelector("#eyeClose");
-let eyeO = document.querySelector("#eyeOpen");
-let pass= document.querySelector("input[type='password']");
-let inpPass = document.querySelector("inpPass");
-let eyeClose = document.querySelector(".fa-eye-slash");
-let eyeOpen = document.querySelector(".fa-eye2");
-eyeC.addEventListener("click",function(){
-    console.log("clicked");
-    pass.type="text";
-    eyeClose.classList.remove("fa-eye-slash");
-    eyeOpen.classList.add("fa-eye");
-});
-eyeO.addEventListener("click",function(){
-    console.log("clicked");
-    pass.type="password";
-    eyeOpen.classList.remove("fa-eye");
-    eyeClose.classList.add("fa-eye-slash");
-});
+let input = document.getElementById("input");
+let span = document.querySelector(".passEye");
+let i = document.querySelector(".passEye i");
+
+function pass (){
+        if(input.type == "password"){
+            input.type = "text";
+            span.innerHTML = `<i class="fa-regular fa-eye"></i>`;
+            
+    }else{
+        input.type = "password";
+        span.innerHTML = `<i class="fa-regular fa-eye-slash"></i>`;
+    }
+ }
